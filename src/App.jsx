b95d9cd38527581e833a6842002081a61fa7f08a
Alanmp1857 <alan.panicker-bvcoel@bvp.edu.in>
2023-02-19 12:28:51 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 function App() {
   return (
     <div className='App'>
       <SignIn />
+      <SignUp />
     </div>
   )
 }
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import { auth } from '../../firebase'
-import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js'
+// import { createWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js'
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-const SignIn = () => {
+const SignUp = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const signIn = (e) => {
+    const signUp = (e) => {
         e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
             console.log(userCredential)
         }).catch((err) => {
             console.log("ERR", err);
@@ -17,14 +18,14 @@ const SignIn = () => {
 
     return (
         <div className='container'>
-            <form onSubmit={signIn}>
-                <h1>Log In to your Account</h1>
+            <form onSubmit={signUp}>
+                <h1>Create Account</h1>
                 <input type="email" placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} />
                 <input type="password" placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type='submit'>Log In</button>
+                <button type='submit'>Sign Up</button>
             </form>
         </div>
     )
 }
 
-export default SignIn
+export default SignUp
